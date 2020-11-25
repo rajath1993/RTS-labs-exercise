381913db5a85d56d4ed7c71861607590058bc0b0
Rajath Anand <anand.rajath@gmail.com>
2020-11-24 20:32:37 -0500

    Second problem - Rotate String by a given number

---
+++ RTS-labs/src/com/rts/exercise/RotateString.java
@@ -0,0 +1,36 @@
+package com.rts.exercise;
+
+import java.util.Scanner;
+/*
+ * #2  Rotate the characters in a string by a given input and have the overflow appear at the beginning, 
+ * e.g. “MyString” rotated by 2 is “ngMyStri”.
+ * */
+
+public class RotateString {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter the string to be rotated: ");
+		String inputString = reader.nextLine();
+		if(inputString.equals("")) {
+			System.out.println("String cannot be empty!");
+			return;
+		}
+		System.out.println("Enter a number to rotate the string: ");
+		int num = reader.nextInt();
+		
+		rotateString(inputString,num);
+	}
+	
+	/*overflow appears at the beginning, made use of modulus to handle it.*/
+	public static void rotateString(String inputString,int num) {
+		String res = "";
+		String prefix = inputString.substring(inputString.length()-(num%inputString.length()), inputString.length());
+		String suffix = inputString.substring(0,inputString.length()-(num%inputString.length()));
+		
+		res = prefix+suffix;
+		
+		System.out.println(res);
+	}
+
+}
